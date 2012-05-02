79afcaa85dbedd87e0c6d0e827152f6293edfb9c
Aaron Price <aprice@bettertheworld.com>
2012-05-02 11:16:17 -0400

    Scaffold users with validation.

---
+++ app/Model/User.php
@@ -0,0 +1,43 @@
+<?php
+
+class User extends AppModel {
+	
+	public $validate = array( 
+						'email' => array( 
+							'email' => array( 
+								'rule' => 'email', 
+								'message' => 'must be a valid email address' 
+							), 
+							'maxLength' => array(
+								'rule' => array( 'maxLength', 60 ),
+								'message' => 'must be less than 60 characters'
+							)
+						),
+						'password' => array(
+							'minLength' => array( 
+								'rule' => array( 'minLength', 5 ),
+								'message' => 'must be at least 5 characters'
+							 )
+						),
+						'first_name' => array(
+							'maxLength' => array( 
+								'rule' => array( 'maxLength', 30 ),
+								'message' => 'must be less than 30 characters'
+							 ),
+							'notEmpty' => array(
+								'rule' => array( 'notEmpty', true ),
+								'message' => 'cannot be empty'
+							)
+						),
+						'last_name' => array(
+							'maxLength' => array( 
+								'rule' => array( 'maxLength', 30 ),
+								'message' => 'must be less than 30 characters'
+							 ),
+							'notEmpty' => array(
+								'rule' => array( 'notEmpty', true ),
+								'message' => 'cannot be empty'
+							)
+						),
+					   );
+}
