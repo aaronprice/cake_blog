d843bd3fb9d748d90a91a91c4a22f2bba85bf2a9
Aaron Price <aprice@bettertheworld.com>
2012-05-02 12:02:11 -0400

    Add model for posts with validation.

---
+++ app/Model/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+class Post extends AppModel {
+	
+	public $name = 'Post';
+	public $validation = array(
+		'title' => array(
+			'maxLength' => array(
+				'rule' => array( 'maxLength', 50 ),
+				'message' => 'cannot be more than 50 characters'
+			),
+			'notEmpty' => array(
+				'rule' => array( 'notEmpty', true ),
+				'message' => 'cannot be empty'
+			)
+		), 
+		'body' => array(
+			'notEmpty' => array(
+				'rule' => array( 'notEmpty', true ),
+				'message' => 'cannot be empty'
+			)
+		)
+	);
+}
