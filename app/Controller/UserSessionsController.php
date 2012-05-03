f3fceec38f4769cbdd22280dc4ca41e75baa7eb5
Aaron Price <aprice@bettertheworld.com>
2012-05-02 21:09:52 -0400

    Use class registry instead of load model.

--- app/Controller/UserSessionsController.php
+++ app/Controller/UserSessionsController.php
@@ -4,11 +4,9 @@ class UserSessionsController extends AppController {
 	
 	public function add() {
 		
-		Controller::loadModel( 'User' );
-		
 		if ( $this->request->is( 'post' ) ) {
 			
-			$User = $this->User->readByEmail( $this->request->data[ 'User' ][ 'email' ] );
+			$User = ClassRegistry::init( 'User' )->readByEmail( $this->request->data[ 'User' ][ 'email' ] );
 			
 			if ( $User && $User->isAuthentic( $this->request->data[ 'User' ][ 'password' ] ) ) {
 				$this->redirect('/');
