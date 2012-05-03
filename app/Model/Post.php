2bddc9a13f7d6ed83889c1250173063a8663e3c6
Aaron Price <aprice@bettertheworld.com>
2012-05-03 08:58:46 -0400

    Has one should really be belongs to.

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -4,12 +4,7 @@ class Post extends AppModel {
 	
 	public $name = 'Post';
 	
-	public $hasOne = array(
-		'User' => array( 
-			'className' => 'User', 
-			'dependent' => true 
-		)
-	);
+	public $belongsTo = array( 'User' );
 	
 	public $whitelist = array( 'title', 'body' );
 	
